a62b5ad2ea9a37ce5f7211a12079f567a147f513
Brian P <162599580+jbp2403@users.noreply.github.com>
2025-09-17 16:20:35 -0500

    Refined logic and output

--- base-images.sh
+++ base-images.sh
@@ -4,7 +4,8 @@ VERBOSE=0
 TOKEN=""
 ENDPOINT="images"
 FILE_NAME=$(date "+%Y%m%d")
-HEADERSTMPFILE="headers.tmp"
+BASEFILTER="false"
+HEADERSTMPFILE="$FILE_NAME-headers.tmp"
 
 #Check requirments
 if ! command -v jq &> /dev/null
@@ -13,7 +14,7 @@ then
     exit 1
 fi
 
-while getopts "vt:e:f:c:" opt; do
+while getopts "vt:e:f:c:b:" opt; do
     case ${opt} in
     v )
     VERBOSE=1
@@ -30,6 +31,9 @@ while getopts "vt:e:f:c:" opt; do
     c )
     CONSOLEURI=$OPTARG
     ;;
+    b )
+    BASEFILTER=$OPTARG
+    ;;
     \? )
     echo "Invalid option: -$OPTARG" >&2
     exit 1
@@ -45,7 +49,7 @@ done
 # Shift off the options so that $1, $2, etc., refer to positional arguments
 shift $((OPTIND -1))
 
-# Now you can use the variables in your script logic
+# Output variable assignment
 echo "Verbose: $VERBOSE"
 echo "File Path: $FILE_NAME"
 echo "Console URI: $CONSOLEURI"
@@ -56,16 +60,14 @@ echo "Remaining arguments: $@"
 
 #Execute curl command and capture the content.
 LIMIT=50
+FIRSTPASS=1
 DELAY_SECONDS=1 # Delay between requests to respect the rate limit
 
-#echo "Calling curl for $CONSOLEURI and Endpoint $ENDPOINT and will write to $FILE_NAME"
+echo "Calling curl for $CONSOLEURI and Endpoint $ENDPOINT and will write scan results to $FILE_NAME"
 
 #Make the initial API call and extract headers for response count
-curl -s -D $HEADERSTMPFILE -o "$FILE_NAME-offset-0.json" -L "$CONSOLEURI/api/v1/$ENDPOINT?limit=$LIMIT&offset=0" -H "Authorization: Bearer $TOKEN" -H "Accept: application/json"
-if [[ $? > 0 ]]; then
-    echo "Initial curl command failed. Exiting..."
-    exit 1
-fi
+curl -s -D $HEADERSTMPFILE -o "$FILE_NAME-offset-0.json" -L "$CONSOLEURI/api/v1/$ENDPOINT?limit=$LIMIT&offset=0&filterBaseImage=$BASEFILTER" -H "Authorization: Bearer $TOKEN" -H "Accept: application/json"
+#HEADERS=$(cat "$HEADERSTMPFILE")
 TOTALCOUNT=$(cat $HEADERSTMPFILE | grep 'Total-Count' |cut -d':' -f2 |xargs |tr -d '\r')
 
 # Check if we successfully got a number
@@ -88,7 +90,7 @@ if (( TOTALCOUNT > LIMIT )); then
         echo "Fetching data with offset: $offset"
         
         # Perform the curl request and save the response.
-        curl -s -o "$FILE_NAME-offset-${offset}.json" -L "$CONSOLEURI/api/v1/$ENDPOINT?limit=$LIMIT&offset=${offset}" -H "Authorization: Bearer $TOKEN" -H "Accept: application/json"
+        curl -s -o "$FILE_NAME-offset-${offset}.json" -L "$CONSOLEURI/api/v1/$ENDPOINT?limit=$LIMIT&offset=${offset}&filterBaseImage=$BASEFILTER" -H "Authorization: Bearer $TOKEN" -H "Accept: application/json"
         
         # Pause to respect the API rate limit
         sleep "$DELAY_SECONDS"
@@ -99,5 +101,6 @@ else
     echo "Total count is less than or equal to the limit. No additional requests needed."
 fi
 
-#Filter resulting files for any baseImage information
-jq '.[].baseImage | select(. != null)' "$FILE_NAME"-offset*.json
+#Output image association from the retrieved data set
+echo "Printing Image and BaseImage relationship where it exists"
+jq -r '.[] |select(. != null) | "Image=\(._id);baseImage=\(.baseImage)"' "$FILE_NAME"-offset*.json
